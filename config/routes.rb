18f665014f910a353478c10d2887690da1368838
Andrei Dashkevich <andrew.daskhevich@yandex.ru>
2018-03-13 08:40:59 +0300

    add auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  post 'auth/send_sms'
+
+  post 'auth/verify_code'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
