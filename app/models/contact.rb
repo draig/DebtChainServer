bc87e2a50a6de1fe3d505546d4fd039660767138
Andrei Dashkevich <andrew.daskhevich@yandex.ru>
2018-03-26 08:35:14 +0300

    add useful method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :friend, class_name: 'User', optional: true
   belongs_to :mapped, class_name: 'User', optional: true
+
+  def find_by_phone(phone)
+    Contact.where("phones = LIKE '%?%' AND mapped_id IS NULL", phone)
+  end
 end
